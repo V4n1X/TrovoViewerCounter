b06d397a5decdb46b286bb4fb879ddf5431105f1
V4n1X <x451822@gmail.com>
2020-07-14 00:49:03 +0200

    Fixed error not loading any values
    Trovo is blocking my requests, so faked the User Agent.

--- TrovoViewerCounter.py
+++ TrovoViewerCounter.py
@@ -4,7 +4,8 @@
 # Last update: 28.06.2020
 # Extracting viewers from trovo.live
 # using web request (#<span data-v-3dfc5312>X viewers</span>)
-# Updates every 2 sec
+# Updates every sec
+# FAKE UserAgent: Win 10 x64 - MS Edge
 
 #Import everything
 from bs4 import BeautifulSoup
@@ -31,7 +32,13 @@ def loopit():
     try:
         #Get data pool
         http = urllib3.PoolManager()
-        r = http.request('GET', url)
+        r = http.request(
+            'GET', 
+            url,
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.61'
+            }
+        )
         
         #Extract items
         soup = BeautifulSoup(r.data, "html.parser")
@@ -50,8 +57,8 @@ def loopit():
     except: #Catch *all* exceptions
         e = sys.exc_info()[0]
     
-    #Wait 60 sec
-    time.sleep(2)
+    #Wait 1 sec
+    time.sleep(1)
 
 #Restart loop
 try:
